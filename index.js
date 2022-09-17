922ef56cccf67e8e5a91ca0713acb94abff41a13
DEAD-STOP <arstop98@gmail.com>
2022-09-17 16:29:59 -0500

    adjusted q

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const questions = [
       {
         type: 'list',
         name: 'license',
-        message: 'What kind of license should your project have?',
+        message: 'What is the license of this project?',
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
       },
       {
