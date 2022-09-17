e589d89cac56fa767fcc88e73781aa8a31bd5a8f
DEAD-STOP <arstop98@gmail.com>
2022-09-17 16:17:59 -0500

    license functions complete

--- index.js
+++ index.js
@@ -6,24 +6,57 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
+        name: 'github',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your GitHub project? Please write a short description.'
+    },    
+    {
+        type: 'input',
         name: 'title',
         message: 'What is the title of your GitHub project?'
     },
     {
         type: 'input',
         name: 'description',
-        message: 'What is your GitHub project? Please write a short description.'
-    },
-    {
+        message: 'Could you write a short description of your project?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
         type: 'input',
-        name: '',
-        message: "What is the title of your GitHub project?"
-    }, 
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',
+      }, 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // fs.writeFile(, fileName, data);
+    fs.writeFile(, fileName, data);
     // need path
 };
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if(license == '' || 'None'){
     return '';
   }else{
-    return `GitHub Liscense -- \n`
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg) \n`
   }
 };
 
@@ -14,13 +14,19 @@ function renderLicenseLink(license) {
   if(license == '' || 'None'){
     return '';
   }else{
-    return `GitHub Liscense -- \n`
+    return `[License](#license)\n`
   }
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license == '' || 'None'){
+    return '';
+  }else{
+    return `This project is liscensed under the ${license} license.\n`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
